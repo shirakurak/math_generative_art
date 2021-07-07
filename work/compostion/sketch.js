5421af37bd308094b39376d27d066559ef1f6757
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2021-07-07 22:46:02 +0900

    Update

--- work/compostion/sketch.js
+++ work/compostion/sketch.js
@@ -1,4 +1,6 @@
 
+float angle;
+
 function setup() {
   createCanvas(480, 340);
   frameRate(1);
@@ -7,11 +9,17 @@ function setup() {
 
 function draw() {
 
-  x = random(width);
-  y = random(height);
-  w = random(0, 20);
+  // 原点をウィンドウの中心に
+  translate(width/2, height/2);
 
-  line(x, y, x+2*w, y);
-  line(x+w, y-w, x+w, y+w);
+  // angle = random(0, 360);
+  print(rad(60));
+
+  // x = random(width);
+  // y = random(height);
+  // w = random(0, 20);
+  //
+  // line(x, y, x+2*w, y);
+  // line(x+w, y-w, x+w, y+w);
 
 }

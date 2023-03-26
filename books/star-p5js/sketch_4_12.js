abd80c2c2839e0ad573cf08e761c018ebb157f67
shirakurak <surakura@hotmail.co.jp>
2023-03-27 08:17:44 +0900

    feat: ch4

---
+++ books/star-p5js/sketch_4_12.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(480, 120)
+  fill(255);
+  stroke(102);
+}
+
+function draw() {
+  background(0);
+  for (var y = 20; y <= height-20; y += 10) {
+    for (var x = 20; x <= width-20; x += 10) {
+      ellipse(x, y, 4, 4);
+      line(x, y, 240, 60);
+    }
+  }
+}
